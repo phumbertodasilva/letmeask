1a072ab5a5c809e755fddfd19a8ceb25ec4854bf
Paulo Humberto <phumbertodasilva@gmail.com>
2021-06-21 17:25:22 -0300

    Iniciando NLW ReactJS

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import {useState} from "react";
+
+export function Button() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={increment}>
+      {count}
+    </button>
+  )
+}
